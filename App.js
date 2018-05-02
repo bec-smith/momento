2582cb39a681699c73731475cf0971a04ed57b3e
Bec <rsmith17@stanford.edu>
2018-05-02 01:01:00 -0700

    navigation
    there's now a way to communicate between the create moment screen and the home screen!

--- App.js
+++ App.js
@@ -7,30 +7,36 @@ import Timeline from 'react-native-timeline-listview'
 class HomeScreen extends React.Component {
   constructor(){
     super()
-    this.state = { data: [
-      {time: '09:00', title: 'Archery Training', description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ', circleColor: '#009688',lineColor:'#009688'},
-      {time: '10:45', title: 'Play Badminton', description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.'},
-      {time: '12:00', title: 'Lunch'},
-      {time: '14:00', title: 'Watch Soccer', description: 'Team sport played between two teams of eleven players with a spherical ball. ',lineColor:'#009688'},
-      {time: '16:30', title: 'Go to Fitness center', description: 'Look out for the Best Gym & Fitness Centers around me :)', circleColor: '#009688'}
-    ]
+    this.state =
+      {
+        data:
+          [
+              {time: '09:00', title: 'Archery Training', description: 'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ', circleColor: '#009688',lineColor:'#009688'},
+              {time: '10:45', title: 'Play Badminton', description: 'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.'},
+              {time: '12:00', title: 'Lunch'},
+              {time: '14:00', title: 'Watch Soccer', description: 'Team sport played between two teams of eleven players with a spherical ball. ',lineColor:'#009688'},
+              {time: '16:30', title: 'Go to Fitness center', description: 'Look out for the Best Gym & Fitness Centers around me :)', circleColor: '#009688'}
+         ]
+     }
   }
-  }
-  static navigationOptions = {
-  header: null,
 
-};
 
 insertNewTime= (time) =>{
-       this.setState({
-           selectedTags: this.state.data.push(time)
-       })
+  this.setState({ data: [...this.state.data, time] })
    }
 
+
   render() {
+    const { params } = this.props.navigation.state;
+    const eventParam = params ? params.eventParam : null; //if (params) params.otherParam,  else null
+    const content = eventParam !== null ? (eventParam) =>  this.insertNewTime(eventParam) : null //supposed to auto add new event
+    console.log(content)
     return (
 
       <View style={styles.container}>
+
+
+      <Text>otherParam: {JSON.stringify(eventParam)}</Text> //shows user inputted moment as json
 
 
       <View style={{
@@ -55,17 +61,29 @@ insertNewTime= (time) =>{
 
         <View style={styles.buttons}>
 
+
         <TouchableHighlight
-        onPress={() => this.props.navigation.navigate('Details')}
-        style = {styles.button}
+        onPress={() => {this.insertNewTime(eventParam)  }}
+        style={styles.button}
         >
 
         <FontAwesome
-          size={40}
-          name='plus-circle'
+          size={20}
+          name='calendar'
           color='#f50'/>
-
         </TouchableHighlight>
+
+        <TouchableHighlight
+        onPress={() => {this.props.navigation.navigate('Details');}}
+        style = {styles.button}
+        >
+
+     <FontAwesome
+       size={20}
+       name='plus-circle'
+       color='#f50'/>
+
+     </TouchableHighlight>
 
         </View>
 
@@ -75,33 +93,24 @@ insertNewTime= (time) =>{
 
 }
 
-// click this button - add random thing to timeline , just checking if it was possible to do this
-// <TouchableHighlight
-// //onPress={() => Alert.alert('You clicked add!') }
-// onPress = {() => {this.insertNewTime("{time: '15:00', title: 'Lunch'}")  }}
-// //underlayColor =  "purple" //color it turns when pressed
-// style = {styles.button}
-// >
-//
-// <FontAwesome
-//   size={40}
-//   name='calendar'
-//   color='#f50'/>
-//
-// </TouchableHighlight>
 
+//DetailsScreen AKA make a moment screen
 class DetailsScreen extends React.Component {
-  static navigationOptions = {
-  header: null,
-};
+
   render() {
+    let momentObject = {time:"10:45", title:'newEvent', description:'the description'};
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Add A Moment Screen</Text>
 
         <Button
           title="Go back"
-          onPress={() => this.props.navigation.goBack()}
+          onPress={() => {
+            this.props.navigation.navigate('Home', {
+              eventParam: momentObject,
+            });
+          }}
         />
       </View>
     );
@@ -119,6 +128,9 @@ const RootStack = StackNavigator(
   },
   {
     initialRouteName: 'Home',
+    headerMode: 'none',
+    mode: 'modal', //makes screen slide up
+
   }
 );
 
@@ -147,6 +159,7 @@ list: {
 },
 buttons: {
   alignItems: 'flex-end',
+  flexDirection: 'row',
   justifyContent: 'center',
 },
 button: {
