a8fcf022e169051b3b0776a70843e43b171b6d2f
Neven Wang-Tomic <nevenwt@gmail.com>
2018-05-17 01:18:45 -0700

    Boilerplate

---
+++ CreateAccount.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, Image, View, Button, ScrollView } from 'react-native';
+
+class CreateAccount extends React.Component {
+
+  render() {
+    
+  }
+}
